d12828e6248df92723eedc9d0755aa31e7a4cae4
Jianwei Han <hanjianwei@gmail.com>
2024-07-12 20:36:59 +0800

    Update install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 if [ ! -f $HOME/.local/bin/micromamba ]; then
-  "${SHELL}" <(curl -L micro.mamba.pm/install.sh)
-  mkdir -p $HOME/micromamba
+  ./micromamba-releases/install.sh
+  mkdir -p $HOME/Applications/micromamba
 fi
