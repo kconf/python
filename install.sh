03ff9a1dfd9d9fa613d643d3d1d0c8ea39eb46ff
Jianwei Han <hanjianwei@gmail.com>
2024-07-19 11:45:47 +0800

    Add pipx and poetry

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ if [ ! -f $HOME/.local/bin/micromamba ]; then
   ./micromamba-releases/install.sh
   mkdir -p $HOME/Applications/micromamba
 fi
+
+sudo pacman -S --needed --noconfirm python-pipx
+pipx install poetry
