47574de91f5ada6417e4912b34548e5a8283c344
Jianwei Han <hanjianwei@gmail.com>
2025-01-30 21:26:17 +0800

    feat(app): Add rye
    Changelog: feature

--- .zsh.d/python.sh
+++ .zsh.d/python.sh
@@ -1,3 +1,6 @@
 export MAMBA_ROOT_PREFIX=~/Applications/micromamba
 eval "$(~/.local/bin/micromamba shell hook -s zsh)"
 micromamba activate base
+
+export RYE_HOME=$HOME/Applications/rye
+source $RYE_HOME/env
