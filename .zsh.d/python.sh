05cc6ca6141e0955763b6f5d9fcb93ffe2e954ad
Jianwei Han <hanjianwei@gmail.com>
2025-03-09 11:14:05 +0800

    feat(app): Check python path
    Changelog: feature

--- .zsh.d/python.sh
+++ .zsh.d/python.sh
@@ -1,6 +1,10 @@
-export MAMBA_ROOT_PREFIX=~/Applications/micromamba
-eval "$(~/.local/bin/micromamba shell hook -s zsh)"
-micromamba activate base
+if [[ -f ~/.local/bin/micromamba ]]; then
+  export MAMBA_ROOT_PREFIX=~/Applications/micromamba
+  eval "$(~/.local/bin/micromamba shell hook -s zsh)"
+  micromamba activate base
+fi
 
-export RYE_HOME=$HOME/Applications/rye
-source $RYE_HOME/env
+if [[ -d $HOME/Applications/rye ]]; then
+  export RYE_HOME=$HOME/Applications/rye
+  source $RYE_HOME/env
+fi
