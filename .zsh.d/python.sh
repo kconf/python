f07cdd3b8651312791c2e2f1171755e9eca8ef86
Jianwei Han <hanjianwei@gmail.com>
2025-03-21 09:13:18 +0800

    feat(app): micromamba -> rye
    Changelog: feature

--- .zsh.d/python.sh
+++ .zsh.d/python.sh
@@ -1,10 +1,5 @@
-if [[ -f ~/.local/bin/micromamba ]]; then
-  export MAMBA_ROOT_PREFIX=~/Applications/micromamba
-  eval "$(~/.local/bin/micromamba shell hook -s zsh)"
-  micromamba activate base
-fi
-
-if [[ -d $HOME/Applications/rye ]]; then
-  export RYE_HOME=$HOME/Applications/rye
+RYE_HOME=$HOME/.rye
+if [[ -d $RYE_HOME ]]; then
   source $RYE_HOME/env
+  export RYE_HOME
 fi
